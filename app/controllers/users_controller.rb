e36769c4785f0174f13cff8e6fcf6b8864b7c75b
inuyashaaa <huymanhtmhp@gmail.com>
2017-06-05 10:46:04 +0700

    Chapter 7 Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".welcome"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".error"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      render file: "public/404.html", status: :not_found, layout: false
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
