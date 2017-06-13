b091988595f5cfa8e4f6a083a72e74e263bd28b0
inuyashaaa <huymanhtmhp@gmail.com>
2017-06-13 11:12:58 +0700

    Chapter10 Update User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
       flash[:success] = t ".welcome_back"
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = t ".error"
       render :new
